90187080dbcd8b4cc929388f2d19f4800d955d44
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-12-11 15:29:53 +0530

    created a server send message and recieved message

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// import
 const ws_1 = require("ws");
-//create
-const wss = new ws_1.WebSocketServer({ port: 8000 });
-//event handler
+const wss = new ws_1.WebSocketServer({ port: 8080 });
 wss.on("connection", function (socket) {
-    console.log("User connected");
-    socket.send("Hello sir !!");
-    setInterval(() => {
-        socket.send("current price of solana is " + Math.random());
-    }, 500);
+    console.log("Connected user !!");
+    socket.on("message", (e) => {
+        console.log(e.toString());
+    });
 });
